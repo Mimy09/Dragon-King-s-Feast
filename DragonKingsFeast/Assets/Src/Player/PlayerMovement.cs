16893d0a7f58e6c3dd0b4e5f3442298ee2917659
Unknown <callum.dunstone@gmail.com>
2018-08-07 11:24:01 +1000

    Removed Example Scenes

--- DragonKingsFeast/Assets/Src/Player/PlayerMovement.cs
+++ DragonKingsFeast/Assets/Src/Player/PlayerMovement.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Player))]
 public class PlayerMovement : MonoBehaviour {
 
+    //valuse that we use to figure out when to start slowing the players directional movemeant
+    public float horizontalBounds;
+    public float verticalBounds;
+
+    //the base speed that we are moving the player forward
+    public float forwardSpeed;
+
+    //a multiplyer that we modifie the finall speed given to the player
+    public float moveSpeed;
+
+    
 	// Use this for initialization
 	void Start () {
 		
+        
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+
+
 	}
 }
