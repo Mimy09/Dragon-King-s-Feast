5d200ff3d6ab5ea594ba96473636c4a69b9d534f
Unknown <callum.dunstone@gmail.com>
2018-08-07 12:06:41 +1000

    Auto stash before merge of "master" and "origin/master"

--- DragonKingsFeast/Assets/Src/Player/PlayerMovement.cs
+++ DragonKingsFeast/Assets/Src/Player/PlayerMovement.cs
@@ -15,10 +15,14 @@ public class PlayerMovement : MonoBehaviour {
     //a multiplyer that we modifie the finall speed given to the player
     public float moveSpeed;
 
+    //how much the player will move this frame
+    public Vector3 velocity;
+
+    public Vector3 startPos;
     
 	// Use this for initialization
 	void Start () {
-		
+        startPos = transform.position;
         
 
 	}
@@ -29,4 +33,46 @@ public class PlayerMovement : MonoBehaviour {
 
 
 	}
+
+    public void SpeedScale() {
+        
+        if (velocity.z > 0) {
+            //pos
+        }
+        else {
+            
+        }
+
+        if (velocity.y > 0) {
+
+        }
+        else {
+
+        }
+    }
+
+    private void ReadPhoneControls() {
+
+        Vector3 acceleration = new Vector3(0, 0, 0);
+        
+        //////////////////Right, Left/////////////////
+
+        float xHolder = -Input.acceleration.x;
+
+        acceleration.z = xHolder;
+
+        Debug.Log("running, X = " + acceleration);
+
+        //////////////////Up, down/////////////////
+        
+            float tiltvalue = Input.acceleration.z;
+
+            Debug.Log("running, X = " + tiltvalue);
+        
+            acceleration.y = -tiltvalue;
+        
+
+        velocity = (acceleration);
+
+    }
 }
