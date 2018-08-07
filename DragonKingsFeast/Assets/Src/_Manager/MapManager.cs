f7242504243b9accd3fdba99710a7a947f1aad84
Unknown <mjmt997@gmail.com>
2018-08-07 14:51:29 +1000

    Level loader

--- DragonKingsFeast/Assets/Src/_Manager/MapManager.cs
+++ DragonKingsFeast/Assets/Src/_Manager/MapManager.cs
@@ -1,31 +1,72 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//**************************************************************************************/
+// ---- Includes ---- //
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MapManager : MonoBehaviour {
 
+    //**************************************************************************************/
+    // ---- Variables ---- //
+
     bool isLoaded_level1, isLoaded_level2, isLoaded_level3;
+
+    //**************************************************************************************/
+    // ---- Functions ---- //
 
     private void Update() {
         if (GameManager.playerObject != null) {
 
-            // load levels
-            if (GameManager.playerObject.transform.position.z > 0) {
-                LoadLevel(1);
+            // Get Player position
+            Vector3 pos = GameManager.playerObject.transform.position;
+
+            // levels 1
+            if (pos.z >= 0) {
+                
+                // Load level
+                if (pos.z <= 100) {
+                    LoadLevel(1);
+                }
+                
+                // Unload levels
+                else {
+                    UnloadLevel(1);
+                }
+            } else {
+                UnloadLevel(1);
             }
 
-            if (GameManager.playerObject.transform.position.z > 25) {
-                LoadLevel(2);
+            // levels 2
+            if (pos.z >= 25) {
+
+                // Load level
+                if (pos.z <= 200) {
+                    LoadLevel(2);
+                }
+
+                // Unload levels
+                else {
+                    UnloadLevel(2);
+                }
             } else {
                 UnloadLevel(2);
             }
 
-            if (GameManager.playerObject.transform.position.z > 125) {
-                LoadLevel(3);
+            if (pos.z >= 125) {
+
+                // Load level
+                if (pos.z <= 200) {
+                    LoadLevel(3);
+                }
+
+                // Unload levels
+                else {
+                    UnloadLevel(3);
+                }
             } else {
                 UnloadLevel(3);
             }
+
+            
         }
 
     }
@@ -34,28 +75,27 @@ public class MapManager : MonoBehaviour {
         switch (level) {
             case 1:
                 // If level 1 is not loaded then load it in
-                if (isLoaded_level1) {
+                if (!isLoaded_level1) {
                     SceneManager.LoadScene(1, LoadSceneMode.Additive);
                     isLoaded_level1 = true;
                 }
                 break;
             case 2:
                 // If level 2 is not loaded then load it in
-                if (isLoaded_level2) {
+                if (!isLoaded_level2) {
                     SceneManager.LoadScene(2, LoadSceneMode.Additive);
                     isLoaded_level2 = true;
                 }
                 break;
             case 3:
                 // If level 3 is not loaded then load it in
-                if (isLoaded_level3) {
+                if (!isLoaded_level3) {
                     SceneManager.LoadScene(3, LoadSceneMode.Additive);
                     isLoaded_level3 = true;
                 }
                 break;
         }
     }
-
     void UnloadLevel(int level) {
         switch (level) {
             case 1:
