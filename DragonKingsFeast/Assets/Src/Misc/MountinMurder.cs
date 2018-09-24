239ea4b794b77dd2ef6855d3bb85c256865d75d8
Unknown <callum.dunstone@gmail.com>
2018-09-24 16:16:57 +1000

    Item Works & Mountain Murder

---
+++ DragonKingsFeast/Assets/Src/Misc/MountinMurder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountinMurder : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            GameManager.player.TakeDamage(1000000);
+        }
+        if (other.tag == "Enemy") {
+            other.GetComponent<Enemy>().TakeDamage(1000000);
+        }
+    }
+}
