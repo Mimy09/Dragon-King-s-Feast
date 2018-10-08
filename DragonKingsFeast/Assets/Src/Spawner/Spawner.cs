24f9cb946c242e5f5829bcc0f3a458b78be392cf
Unknown <callum.dunstone@gmail.com>
2018-10-09 10:39:16 +1100

    Added in spawner variables

--- DragonKingsFeast/Assets/Src/Spawner/Spawner.cs
+++ DragonKingsFeast/Assets/Src/Spawner/Spawner.cs
@@ -9,4 +9,12 @@ public enum e_FormationType {
 
 public class Spawner : MonoBehaviour {
 
+    public e_FormationType formation;
+    public List<e_EnemyType> wingOfEnemies;
+    private List<Enemy> listOfEnemies;
+
+    public List<Enemy> GetListOfEnemies() {
+        return listOfEnemies;
+    }
+
 }
