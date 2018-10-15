0cd6ee58aa572985d77a1f9998375096b7c3ed8f
Unknown <mjmt997@gmail.com>
2018-10-15 11:44:54 +1100

    coin fix

--- DragonKingsFeast/Assets/Src/Item/Item.cs
+++ DragonKingsFeast/Assets/Src/Item/Item.cs
@@ -14,20 +14,23 @@ public class Item : MonoBehaviour {
     public int value = 1;
     public e_ItemType m_itemType;
     public e_ItemType ItemType { get { return m_itemType; } }
-
+    public bool spawner = false;
+    
     public virtual void TurnOff() {
         Reset();
         GameManager.instance.GetObjectPool().AddItemTooPool(this);
-        GameManager.itemList.Add(gameObject);
+        GameManager.itemList.Remove(gameObject);
     }
     public virtual void TurnOn() {
         Reset();
-        GameManager.itemList.Remove(gameObject);
+        GameManager.itemList.Add(gameObject);
         this.gameObject.SetActive(true);
     }
     public virtual void Reset() { }
 
     public void OnTriggerEnter(Collider other) {
+        if (spawner == false) return;
+
         if (other.tag == "Player") {
             if (m_itemType != e_ItemType.Pickup) {
                 GameManager.player.ApplyBoost(m_itemType);
@@ -50,8 +53,21 @@ public class Item : MonoBehaviour {
     }
 
     public void Update() {
+        if (spawner == false) Debug.LogError("WRONG SPAWN!!!!!!!!!!!!");
+
+        Vector3 r = transform.rotation.eulerAngles;
+        r.y += Time.deltaTime * 90;
+        transform.rotation = Quaternion.Euler(r);
+
         if (transform.position.z < (GameManager.player.transform.position.z - 5)) {
             TurnOff();
+        }
+    }
+
+    private void OnDrawGizmos() {
+        if (spawner == false) {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawCube(transform.position, new Vector3(3, 3, 3));
         }
     }
 }
---
+++ DragonKingsFeast/Assets/Src/Item/SpawnItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItem : MonoBehaviour {
+    public e_ItemType itemType;
+    public int lootValue = 1;
+    public Mesh gizmosMeshCoin;
+    public Mesh gizmosMeshBoost;
+    
+
+    void Start () {
+        Item loot = GameManager.instance.GetObjectPool().FindItemOfType(itemType).GetComponent<Item>();
+        loot.spawner = true;
+        loot.transform.position = transform.position;
+        loot.transform.rotation = transform.rotation;
+        loot.value = lootValue;
+
+        Destroy(this);
+    }
+	
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+
+        if (itemType == e_ItemType.Pickup)
+            Gizmos.DrawMesh(gizmosMeshCoin, transform.position, transform.rotation, new Vector3(0.2f, 0.5f, 0.5f));
+        else {
+            if(itemType == e_ItemType.Boost_Attack) Gizmos.color = Color.red;
+            else if (itemType == e_ItemType.Boost_Defense) Gizmos.color = Color.green;
+            else if (itemType == e_ItemType.Boost_Speed) Gizmos.color = Color.blue;
+            Gizmos.DrawMesh(gizmosMeshBoost, transform.position, Quaternion.identity, new Vector3(1, 1, 1));
+        }
+    }
+}
