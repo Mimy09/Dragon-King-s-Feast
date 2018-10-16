b2fc4b783ccd6ac7ced69938b4bf89aafba3987e
Unknown <mjmt997@gmail.com>
2018-10-16 12:51:34 +1100

    WaterSpout

---
+++ DragonKingsFeast/Assets/Src/Misc/WaterSpout.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSpout : MonoBehaviour {
+
+    public float speed  = 1;
+    public float height = 10;
+    public float bias   = 50;
+    Transform player;
+
+	void Update () {
+        if (player == null) {
+            if (GameManager.player == null) return;
+            player = GameManager.player.transform;
+        }
+
+        Vector3 t = transform.position;
+        float dist = Vector3.Distance(t, player.position);
+
+        if (dist < bias)
+            if (t.y < height)
+                t.y += Time.deltaTime * speed;
+	}
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position + (transform.up * height), 1);
+    }
+}
