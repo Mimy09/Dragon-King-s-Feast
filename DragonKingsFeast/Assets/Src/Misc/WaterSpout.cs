c3780f193c36d78366ee1fe089d29ed0a6eca5be
Unknown <callum.dunstone@gmail.com>
2018-10-24 10:32:08 +1100

    Watter spout partical effects

--- DragonKingsFeast/Assets/Src/Misc/WaterSpout.cs
+++ DragonKingsFeast/Assets/Src/Misc/WaterSpout.cs
@@ -9,18 +9,37 @@ public class WaterSpout : MonoBehaviour {
     public float bias   = 50;
     Transform player;
 
-	void Update () {
+    public GameObject bubbles;
+    public GameObject spout;
+    public Transform p;
+
+    private GameObject hodler;
+
+    private void Start() {
+        hodler = Instantiate(bubbles, p.position, Quaternion.identity);
+    }
+
+    void Update () {
         if (player == null) {
             if (GameManager.player == null) return;
             player = GameManager.player.transform;
         }
 
         Vector3 t = transform.position;
-        float dist = Vector3.Distance(t, player.position);
+        float dist = t.z - player.position.z;
 
         if (dist < bias)
-            if (t.y < height)
+            if (t.y < height) {
                 t.y += Time.deltaTime * speed;
+
+                if (hodler != null) {
+                    Destroy(hodler, 1.0f);
+                    hodler = null;
+                    Destroy(Instantiate(spout, p.position, Quaternion.identity), 3.0f);
+                }
+            }
+
+        transform.position = t;
 	}
 
     private void OnDrawGizmos() {
